ca63189628a66b974b4e61d13576ae72fc620d78
Daniel Thoo <daniel.thoo21@gmail.com>
2022-11-06 20:48:45 -0500

    Fixed Valid Parenthesis question

--- valid_parenthesis.java
+++ valid_parenthesis.java
@@ -5,8 +5,6 @@ import java.util.NoSuchElementException;
 class Solution {
     public boolean isValid(String s) {
         Deque<Integer> bracketStack = new ArrayDeque<>();
-        Deque<Integer> sqBracketStack = new ArrayDeque<>();
-        Deque<Integer> wavyBracketStack = new ArrayDeque<>();
 
         for (int i = 0; i < s.length(); i++) {
             char bracket = s.charAt(i);
@@ -15,30 +13,42 @@ class Solution {
                 if ('(' == bracket) {
                     bracketStack.push(0);
                 } else if (')' == bracket) {
-                    bracketStack.pop();
+                    if (bracketStack.peek() == 0) {
+                        bracketStack.pop();
+                    } else {
+                        return false;
+                    }
                 } else if ('[' == bracket) {
-                    sqBracketStack.push(0);
+                    bracketStack.push(1);
                 } else if (']' == bracket) {
-                    sqBracketStack.pop();
+                    if (bracketStack.peek() == 1) {
+                        bracketStack.pop();
+                    } else {
+                        return false;
+                    }
                 } else if ('{' == bracket) {
-                    wavyBracketStack.push(0);
+                    bracketStack.push(2);
                 } else if ('}' == bracket) {
-                    wavyBracketStack.pop();
+                    if (bracketStack.peek() == 2) {
+                        bracketStack.pop();
+                    } else {
+                        return false;
+                    }
                 } else {
                     // Fail in case of invalid character
                     return false;
                 }
-            } catch (NoSuchElementException e) {
+            } catch (Exception e) {
                 // Fail in case of closed bracket before open bracket
                 return false;
             }
         }
 
-        if (bracketStack.isEmpty() && sqBracketStack.isEmpty() && wavyBracketStack.isEmpty()) {
-            // Pass if all stacks are empty
+        if (bracketStack.isEmpty()) {
+            // Pass if stack is empty
             return true;
         } else {
-            // Fail if not all stacks are empty
+            // Fail if stack is not empty
             return false;
         }
     }
