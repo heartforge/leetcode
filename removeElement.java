4e2b4778838a32fd739f52c983147367eca1589f
Daniel Thoo <daniel.thoo21@gmail.com>
2023-08-06 20:10:33 -0400

    Class debuggable locally

--- removeElement.java
+++ removeElement.java
@@ -1,4 +1,4 @@
-class Solution {
+public class RemoveElement {
     public int removeElement(int[] nums, int val) {
         int end = nums.length-1;
         for (int i = 0; i < nums.length; i++) {
@@ -12,5 +12,11 @@ class Solution {
             }
         }
         return end+1;
+    }
+
+    public static void main(String[] args) {
+        RemoveElement element = new RemoveElement();
+        int count = element.removeElement(new int[]{3,2,2,3}, 3);
+        System.out.println(count);
     }
 }
